b458f315b04f1d75b8b8026315af358b911e39bf
Your Name <you@example.com>
2025-02-08 20:43:15 +0100

    Second commit

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,5 @@
-import 'package:flower_app/components/custom_textfield.dart';
+import 'package:flower_app/shared/colors.dart';
+import 'package:flower_app/shared/custom_textfield.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -7,25 +8,51 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(33.0),
           child: Column(
             children: [
-              const SizedBox(height: 64,),
-              MyTextField(
-                textIputTypeee: TextInputType.emailAddress,
-                isPassword: false,
-                hintTexttt: "Entrez Votre Email...",
+              const SizedBox(
+                height: 64,
               ),
-              const SizedBox(height: 32,),
-              MyTextField(
-                textIputTypeee: TextInputType.text,
-                isPassword: true,
-                hintTexttt: "Entrez Votre Password...",
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+                decoration: decorationTextfield.copyWith(
+                  hintText: "Entrez Votre Email...",
+                ),
               ),
-              
+              const SizedBox(
+                height: 32,
+              ),
+              TextField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: decorationTextfield.copyWith(
+                  hintText: "Entrez Votre Password...",
+                ),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:btnGreen, 
+                  padding: EdgeInsets.all(10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: Text(
+                  "Click Here",
+                  style: TextStyle(
+                    fontSize: 19,
+                    color: Colors.white, 
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -33,5 +60,3 @@ class Login extends StatelessWidget {
     );
   }
 }
-
-
